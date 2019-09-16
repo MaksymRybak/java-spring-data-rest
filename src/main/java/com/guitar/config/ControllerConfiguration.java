5cc84563d6d904dae17837968f86a6880c569762
Maksym Rybak <rybak.maksym@gmail.com>
2019-09-16 14:44:04 +0200

    add exception handler for status code 500

--- src/main/java/com/guitar/config/ControllerConfiguration.java
+++ src/main/java/com/guitar/config/ControllerConfiguration.java
@@ -10,9 +10,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ControllerConfiguration {
 	@ExceptionHandler(ConstraintViolationException.class)
-	@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid data sent ")
-	public void notValid() {
-		// possiamo inviare dei messaggi/email a chi di compotenze, quando un erore viene sollevato
-		
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid data sent to the endpoint")
+	public void notValidBadRequest() {
+		// possiamo inviare dei messaggi/email a chi di compotenze, quando un erore viene sollevato		
+	}
+	
+	@ExceptionHandler(IllegalStateException.class)
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Data is not valid")
+	public void notValidInternalServerError() {
+		// possiamo inviare dei messaggi/email a chi di compotenze, quando un erore viene sollevato		
 	}
 }
